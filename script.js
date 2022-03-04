8bfb4f5a3a844e1a9af21c40eb7823fea782b016
twotimesgi <luigibardellagerbi@mail.com>
2022-03-04 14:51:20 +0100

    counter

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ let container = document.querySelector("#container");
 let randTarget = [];
 let guess;
 let win;
+let correctCount;
 function hide(){
     container.innerHTML = "";
 }
 
 function ask(){
     win = true;
+    correctCount = 0;
     for(let i = 1; i <= randTarget.length; i++){
         guess = prompt("Inserisci il " + i + "° numero: ");
         if(randTarget.includes(guess)){
             console.log("Il numero " + guess + " è corretto.");
+            correctCount += 1;
         }else{
             console.log("il numero " + guess + " è errato.")
             win = false;
         }
     }
-    if(win) console.log("Complimenti hai indovinato tutti i numeri, hai vinto!");
+    if(win){
+        console.log("Complimenti hai indovinato tutti i numeri, hai vinto!");
+    }else{
+        console.log("Hai totalizzato " + correctCount + " punti.")
+    }
 }
 
 while (randTarget.length < 5){ 
